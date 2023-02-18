5a4dc4723b15f9860909015a3d0480b4bd57fc72
houssein <badrahsein@gmail.com>
2023-02-19 00:32:26 +0200

    set up the primary store

---
+++ src/features/auth/auth.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const authSlice = createSlice({
+  name: 'login',
+  initialState,
+  reducers: {},
+});
+
+export default authSlice;
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './features/auth/auth';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { auth: authSlice.reducer },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
